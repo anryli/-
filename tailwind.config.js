127e3e3016d3c449be47e8a6410825fc08da73d6
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-07-18 00:16:54 +0900

    add tailwind and daisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ECD189",
+          secondary: "#ff00ff",
+          accent: "#00ffff",
+          neutral: "#ff00ff",
+          "base-100": "#c7d2fe",
+          info: "#0000ff",
+          success: "#00ff00",
+          warning: "#00ff00",
+          error: "#ff0000",
+        },
+      },
+    ],
+  },
+};
